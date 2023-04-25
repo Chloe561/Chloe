70717cffacb008f3831eb866ed61512e70354dfc
Wenjing Li <131727652+Chloe561@users.noreply.github.com>
2023-04-24 19:48:06 -0700

    Update index.php
    For class assignment

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
-
+<!DOCTYPE html>
+<html>
+  
+<head>
+  <title>Chloe Li</title>
+</head>  
+  
+<body>
+  <h1>Hello world, I am Chloe's frist website project!</h1>
+  <p>I'm so excited to see what I can do here</p>
+  <p><em>---Chloe Li</em></p>
+</body>  
+</html>
