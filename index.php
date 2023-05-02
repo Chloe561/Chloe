25786c3a804e2009729768c334c4dfc0201d8160
Chloe Li <131727652+Chloe561@users.noreply.github.com>
2023-05-01 23:44:17 -0700

    Update index.php

--- index.php
+++ index.php
@@ -3,9 +3,9 @@
   
 <head>
   <title>Chloe Li</title>
-</head>  
-  
+</head>    
 <body>
+  <a href="css_demo.html">This is a link to the demo files</a>
   <h1>Hello world, I am Chloe's frist website project!</h1>
   <p>I'm so excited to see what I can do here</p>
   <p><em>---Chloe Li</em></p>
