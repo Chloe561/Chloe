cf548fbf2c90f744b05569108f6ac8c5a125a14c
Chloe Li <131727652+Chloe561@users.noreply.github.com>
2023-05-06 11:39:11 -0700

    Update index.php

--- index.php
+++ index.php
@@ -5,10 +5,12 @@
   <title>Chloe Li</title>
 </head>    
 <body>
-  <p><a href="css_demo.html">This is a link to the demo files</a></p>
-  <p><a href="font_sample.html">This is a link to the font-sample file</a></p>
+  
   <h1>Hello world, I am Chloe's first website project!</h1>
   <p>I'm so excited to see what I can do here</p>
+  <p><a href="css_demo.html">This is a link to the demo files</a></p>
+  <p><a href="font_sample.html">This is a link to the font-sample file</a></p>
+  <p><a href="bootstrap-sample.html">This is a link to the bootstrap-sample file</a></p>
   <p>This is a link to the font-sample file</p>
   <p><em>---Chloe Li</em></p>
 </body>  
