447c14f3455bbca905587de95fc16c65b5fc239c
Chloe Li <131727652+Chloe561@users.noreply.github.com>
2023-05-22 18:40:51 -0700

    Update index.php

--- index.php
+++ index.php
@@ -6,12 +6,13 @@
 </head>    
 <body>
   
-  <h1>Hello, this is a simple link page menu!</h1>
+  <h1>Hello, this is a menu for all the assignments in 285!</h1>
   <p>Links find below</p>
   <p><a href="css_demo.html">This is a link to the demo files</a></p>
   <p><a href="font_sample.html">This is a link to the font-sample file</a></p>
   <p><a href="bootstrap-sample.html">This is a link to the bootstrap-sample file</a></p>
-  <p>This is a link to the font-sample file</p>
+
+  <p><a href="css_demo.html">Reversi Assignment </a></p>
 
 </body>  
 </html>
